b72281118f436978d431bdfb47b3b4217511fb06
Yuqian <yuqian.yang@msn.com>
2026-09-19 16:46:22 -0700

    chore: release v1.0.1

--- scripts/YingChuang.user.js
+++ scripts/YingChuang.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         萤窗
 // @name:zh-TW         螢窗
 // @namespace          https://github.com/JasonEX
-// @version            1.0.0
+// @version            1.0.1
 // @author             JasonEX
 // @description        萤窗：小说阅读脚本，智能正文识别、连续阅读、阅读位置恢复、简繁转换
 // @description:zh-CN  萤窗：小说阅读脚本，智能正文识别、连续阅读、阅读位置恢复、简繁转换
@@ -8687,8 +8687,8 @@
 		else if (options) managerInstance.updateOptions(options);
 		return managerInstance;
 	}
-	var VERSION = "1.0.0";
-	var BUILD_DATE = "2026-09-18";
+	var VERSION = "1.0.1";
+	var BUILD_DATE = "2026-09-19";
 	var SENSITIVE_QUERY_KEY = /(?:^|[_-])(?:token|auth|session|sid|key|sign|signature|ticket|password|passwd|pwd|jwt|credential|access|refresh|challenge|chl)(?:[_-]|$)|^__cf_/i;
 	function redactUrl(url) {
 		if (!url) return null;
